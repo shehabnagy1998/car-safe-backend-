3dfc03cde6d8e5cc8c3cdb8bf53b1e84798c8c74
shehab nagy <shehab.nagy1998@gmail.com>
2019-04-20 23:04:41 -0700

    initial commit v6

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
 const serverMon = 'mongodb+srv://shehab90:shehab80@cluster0-fw0qv.mongodb.net/Software_Project';
 const helpers = require('./routes/helpers');
@@ -9,29 +10,22 @@ const user = require('./routes/user');
 const lost = require('./routes/lost');
 const found = require('./routes/found');
 
-//CORS middleware
-const allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cors({
-//     origin: '*',
-//     optionsSuccessStatus: 200
-// }));
-app.use(allowCrossDomain);
+app.use(cors());
 app.use('/helpers', helpers);
 app.use('/user', user);
 app.use('/lost', lost);
 app.use('/found', found);
+app.use('/', express.static(path.join(__dirname, './build')));
+app.use('/static', express.static(path.join(__dirname, './build/static')));
 
 mongoose.connect(serverMon, { useNewUrlParser: true })
     .then(res => { console.log('database connected') })
     .catch(err => { console.log(err) })
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'))
+})
 
 module.exports = app
---
+++ build/precache-manifest.702d0786f9fd262c622b3bc0834a9578.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f8d32183313b9ac2c9c917461e062e2",
+    "url": "/static/media/sign-background.2f8d3218.jpg"
+  },
+  {
+    "revision": "1a0f165413652158468fe113e864afa4",
+    "url": "/static/media/background-blured.1a0f1654.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d0ba6e755ec0f6c6f57",
+    "url": "/static/js/main.d891fe30.chunk.js"
+  },
+  {
+    "revision": "910a33851038af8e10cc",
+    "url": "/static/js/2.de15a90e.chunk.js"
+  },
+  {
+    "revision": "4d0ba6e755ec0f6c6f57",
+    "url": "/static/css/main.fd0775fc.chunk.css"
+  },
+  {
+    "revision": "910a33851038af8e10cc",
+    "url": "/static/css/2.15e704dd.chunk.css"
+  },
+  {
+    "revision": "3d2d0ca9513ea1f265f0032afb8dbc32",
+    "url": "/index.html"
+  }
+];
